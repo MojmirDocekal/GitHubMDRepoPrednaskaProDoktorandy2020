7fa0726a7323eee702de5cc96b072a8bfc00b318
Mojmir Docekal <docekal@phil.muni.cz>
2020-11-06 09:57:45 +0100

    ch1

--- 01-tidy-text.R
+++ 01-tidy-text.R
@@ -209,4 +209,32 @@ cor(count_alice$n, count_wells$n, method = "spearman")
 
 cor.test(count_alice$n, count_wells$n, method = "spearman", alternative = "greater")
 
+# Capek attempt
+# lemmatizator: http://lindat.mff.cuni.cz/services/morphodita/
 
+library(tidyverse)
+
+capek <- read_lines("capek_rur_lemma.txt")
+
+library(tidytext)
+
+# text_df <- as.tibble(capek)
+
+text_df <- tibble(line = 1:4483, text = capek)
+
+tidy_capek <- text_df %>%
+  unnest_tokens(word, text)
+
+tidy_capek %>%
+  count(word, sort = TRUE)
+
+library(ggplot2)
+
+tidy_capek %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 50) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
